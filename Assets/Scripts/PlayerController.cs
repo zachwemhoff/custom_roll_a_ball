7e1750a6b0ca913b1900677bcd0f0bce23e05c1d
Zach Wemhoff <you@example.com>
2022-02-02 22:21:08 -0600

    First commit custom Roll-a-Ball

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        if(count >= 15)
         {
             winTextObject.SetActive(true);
         }
